54d858eb51b05b5d2ee44af9291b74a391e55fa4
Martin Cajthaml <martin.cajthaml@symbio.agency>
2020-04-12 15:36:28 +0200

    new webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: __dirname + '/src/index.js',
+  entry: `${__dirname}/src/index.js`,
   mode: process.env.NODE_ENV,
   output: {
-    path: __dirname + '/public',
-    filename: 'bundle.js'
+    path: `${__dirname}/public`,
+    filename: 'bundle.js',
   },
   devtool: 'source-map',
   devServer: {
@@ -20,21 +20,21 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        include: __dirname + '/src',
+        include: `${__dirname}/src`,
         loader: 'eslint-loader',
         enforce: 'pre',
       },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        use: { loader: 'babel-loader' }
+        use: { loader: 'babel-loader' },
       },
       {
         test: /\.css$/,
         use: [
-          "style-loader",
-          "css-loader"
-        ]
+          'style-loader',
+          'css-loader',
+        ],
       },
     ],
   },
@@ -43,13 +43,12 @@ module.exports = {
       title: 'DatoCMS Plugin',
       minify: isProduction,
     }),
-    new HtmlWebpackIncludeAssetsPlugin({
-      append: false,
-      publicPath: '',
-      assets: [
+    new HtmlWebpackTagsPlugin({
+      tags: [
         'https:////unpkg.com/datocms-plugins-sdk/dist/sdk.js',
         'https:////unpkg.com/datocms-plugins-sdk/dist/sdk.css',
-      ]
+      ],
+      append: true,
     }),
   ].filter(Boolean),
-}
+};
