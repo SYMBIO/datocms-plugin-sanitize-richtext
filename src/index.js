c8f4c0ce3229118dfa3fb9ba96af470380edc47f
Martin Cajthaml <martin.cajthaml@symbio.agency>
2021-02-10 14:17:55 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ function sanitize(text) {
   if (text) {
     return sanitizeHtml(text, {
       allowedTags: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol', 'li',
-        'b', 'i', 'strong', 'em', 'strike', 'br', 'table', 'thead', 'caption', 'tbody', 'tr', 'th',
-        'td', 'iframe', 'img'],
+        'b', 'i', 'strong', 'em', 'strike', 'br', 'table', 'thead', 'caption', 'tbody', 'tfoot',
+        'tr', 'th', 'td', 'iframe', 'img'],
       allowedAttributes: {
         a: ['href', 'name', 'target', 'rel'],
         iframe: ['src', 'width', 'height', 'data-name'],
